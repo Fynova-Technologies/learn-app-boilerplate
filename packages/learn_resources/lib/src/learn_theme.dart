d499e1b7ec31be4de59b8871482426605bd02eb7
Avishek Subedi <avisheksubedi0@gmail.com>
2025-02-06 11:29:38 +0545

    `Refactor LearnThemeConfig to use custom TextTheme and update ColorSchemes`

--- packages/learn_resources/lib/src/learn_theme.dart
+++ packages/learn_resources/lib/src/learn_theme.dart
@@ -47,61 +47,95 @@ class _LearnThemeScope extends InheritedWidget {
 
 class LearnThemeConfig {
   LearnThemeConfig({required this.mode}) {
-    // var robotoTextTheme = GoogleFonts.robotoTextTheme();
-    // const robotoMF = 'Roboto_medium';
-
-    // robotoTextTheme = robotoTextTheme.copyWith(
-    //   titleMedium: robotoTextTheme.titleMedium!.copyWith(fontFamily: robotoMF),
-    //   titleSmall: robotoTextTheme.titleSmall!.copyWith(fontFamily: robotoMF),
-    //   labelLarge: robotoTextTheme.labelLarge!.copyWith(fontFamily: robotoMF),
-    //   labelMedium: robotoTextTheme.labelMedium!.copyWith(fontFamily: robotoMF),
-    //   labelSmall: robotoTextTheme.labelSmall!.copyWith(fontFamily: robotoMF),
-    // );
-
-    //for text theme
-    // final customTextTheme = TextTheme(
-    //   titleLarge: GoogleFonts.raleway(fontSize: 48),
-    //   titleMedium:
-    //       TextStyle(fontFamily: 'rage', color: Colors.white, fontSize: 18),
-    //   bodyLarge: GoogleFonts.raleway(
-    //       fontSize: 28, fontWeight: FontWeight.w700, color: Colors.white),
-    //   bodySmall: GoogleFonts.raleway(fontSize: 12, color: Colors.blue),
-    //   bodyMedium: GoogleFonts.raleway(
-    //       fontSize: 18, color: Colors.white, fontWeight: FontWeight.w500),
-    //   labelSmall: GoogleFonts.raleway(
-    //     fontSize: 16,
-    //     fontWeight: FontWeight.w400,
-    //     color: Colors.red,
-    //   ),
-    //   displaySmall: GoogleFonts.raleway(
-    //       fontSize: 18, color: Colors.white, fontWeight: FontWeight.w200),
-    //   labelMedium: GoogleFonts.raleway(
-    //     fontSize: 16,
-    //     fontWeight: FontWeight.w400,
-    //     color: Colors.white,
-    //   ),
-    //   labelLarge: GoogleFonts.raleway(
-    //       fontSize: 19, fontWeight: FontWeight.w700, color: Colors.white),
-    // );
+    // Define the text theme based on the typography image
+    final TextTheme customTextTheme = TextTheme(
+      displayLarge: GoogleFonts.roboto(
+        fontSize: 57,
+        fontWeight: FontWeight.w400,
+        letterSpacing: -0.25,
+      ),
+      displayMedium: GoogleFonts.roboto(
+        fontSize: 45,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0,
+      ),
+      displaySmall: GoogleFonts.roboto(
+        fontSize: 36,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0,
+      ),
+      headlineLarge: GoogleFonts.roboto(
+        fontSize: 32,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0,
+      ),
+      headlineMedium: GoogleFonts.roboto(
+        fontSize: 28,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0,
+      ),
+      headlineSmall: GoogleFonts.roboto(
+        fontSize: 24,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0,
+      ),
+      titleLarge: GoogleFonts.roboto(
+        fontSize: 22,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0,
+      ),
+      titleMedium: GoogleFonts.roboto(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.15,
+      ),
+      titleSmall: GoogleFonts.roboto(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.1,
+      ),
+      labelLarge: GoogleFonts.roboto(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.1,
+      ),
+      labelMedium: GoogleFonts.roboto(
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.5,
+      ),
+      labelSmall: GoogleFonts.roboto(
+        fontSize: 11,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.5,
+      ),
+      bodyLarge: GoogleFonts.roboto(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.15,
+      ),
+      bodyMedium: GoogleFonts.roboto(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+      ),
+      bodySmall: GoogleFonts.roboto(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.4,
+      ),
+    );
 
     light = ThemeData(
       colorScheme: _lightColorScheme,
+      textTheme: customTextTheme,
       useMaterial3: true,
-      // textTheme: customTextTheme,
-    ).copyWith(
-      dividerColor: Colors.transparent,
-      tabBarTheme: TabBarTheme(
-        dividerColor: Colors.transparent,
-        indicator: const BoxDecoration(color: Colors.transparent),
-        labelColor: _lightColorScheme.tertiary,
-        unselectedLabelColor: _lightColorScheme.shadow,
-      ),
     );
 
     dark = ThemeData(
       colorScheme: _darkColorScheme,
+      textTheme: customTextTheme,
       useMaterial3: true,
-      // textTheme: customTextTheme,
     );
   }
 
@@ -111,95 +145,46 @@ class LearnThemeConfig {
   late final ThemeData dark;
 }
 
-ColorScheme _lightColorScheme = ColorScheme.light(
-  primary: Colors.black,
-  secondary: Color(0xff4C626B),
-  tertiary: Color(0xff107FC4),
-  error: Color.fromARGB(255, 0, 0, 0),
-  // On Colors
-  onPrimary: Colors.black,
-
-  onSecondary: Color(0xff1D83AF),
-  onTertiary: Colors.white,
-  // Container Colors
-  primaryContainer: Colors.amber,
-  secondaryContainer: Color(0xffCFE6F1),
-  tertiaryContainer: Color(0xffCEE5FF),
-  errorContainer: Color(0xffFFDAD6),
-  // On Container Colors
-  onPrimaryContainer: Color(0xff0261A6),
-  //Selected container
-  onSecondaryContainer: Colors.black12,
-  //UnselectedContainer
-  onTertiaryContainer: Color(0xff001D33),
-  onErrorContainer: Color(0xff410002),
-  // Neutral Colors
-  surface: Color(0xffE3EDF7),
-  surfaceTint: Color(0xffDCE8F5),
-  onSurface: Color(0xff191C1D),
-  onSurfaceVariant: Color(0xff757B80),
-  surfaceContainerHighest: Color(0xffdce4e8),
-  outline: Color(0xff9EA3A6),
-  outlineVariant: Colors.white,
-  // Inverse & Shadow Colors
-  inverseSurface: Color(0xff2E3032),
-  onInverseSurface: Color(0xffE0EAF6),
-  inversePrimary: Color(0xff5FD4FD),
-  scrim: Colors.black,
-  shadow: Colors.black,
+ColorScheme _lightColorScheme = const ColorScheme.light(
+  primary: Color(0xFF00376B),
+  onPrimary: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFFCEE5FF),
+  onPrimaryContainer: Color(0xFF001E42),
+  secondary: Color(0xFF4C626B),
+  onSecondary: Color(0xFFFFFFFF),
+  secondaryContainer: Color(0xFFCFE6F1),
+  onSecondaryContainer: Color(0xFF071F26),
+  tertiary: Color(0xFF006875),
+  onTertiary: Color(0xFFFFFFFF),
+  tertiaryContainer: Color(0xFFAFEBF3),
+  onTertiaryContainer: Color(0xFF001F24),
+  error: Color(0xFFB3261E),
+  onError: Color(0xFFFFFFFF),
+  errorContainer: Color(0xFFF9DEDC),
+  onErrorContainer: Color(0xFF410E0B),
+  surface: Color(0xFFFBFCFD),
+  onSurface: Color(0xFF1C1B1F),
+  outline: Color(0xFF72777D),
 );
 
-const ColorScheme _darkColorScheme = ColorScheme.dark(
-  primary: Colors.black,
-  secondary: Color(0xff4C626B),
-  tertiary: Color(0xff107FC4),
-  error: Color.fromARGB(255, 0, 0, 0),
-  // On Colors
-  onPrimary: Colors.black,
-
-  onSecondary: Color(0xff1D83AF),
-  onTertiary: Colors.white,
-  // Container Colors
-  primaryContainer: Colors.amber,
-  secondaryContainer: Color(0xffCFE6F1),
-  tertiaryContainer: Color(0xffCEE5FF),
-  errorContainer: Color(0xffFFDAD6),
-  // On Container Colors
-  onPrimaryContainer: Color(0xff0261A6),
-  //Selected container
-  onSecondaryContainer: Colors.black12,
-  //UnselectedContainer
-  onTertiaryContainer: Color(0xff001D33),
-  onErrorContainer: Color(0xff410002),
-  // Neutral Colors
-  surface: Color(0xffE3EDF7),
-  surfaceTint: Color(0xffDCE8F5),
-  onSurface: Color(0xff191C1D),
-  onSurfaceVariant: Color(0xff757B80),
-  surfaceContainerHighest: Color(0xffdce4e8),
-  outline: Color(0xff9EA3A6),
-  outlineVariant: Colors.white,
-  // Inverse & Shadow Colors
-  inverseSurface: Color(0xff2E3032),
-  onInverseSurface: Color(0xffE0EAF6),
-  inversePrimary: Color(0xff5FD4FD),
-  scrim: Colors.black,
-  shadow: Colors.black,
+ColorScheme _darkColorScheme = const ColorScheme.dark(
+  primary: Color(0xFF84CFFF),
+  onPrimary: Color(0xFF003354),
+  primaryContainer: Color(0xFF004B76),
+  onPrimaryContainer: Color(0xFFCFE5FF),
+  secondary: Color(0xFFB2CBD3),
+  onSecondary: Color(0xFF1C333B),
+  secondaryContainer: Color(0xFF334B53),
+  onSecondaryContainer: Color(0xFFCFE6F1),
+  tertiary: Color(0xFF6FD5E0),
+  onTertiary: Color(0xFF00363F),
+  tertiaryContainer: Color(0xFF004F5A),
+  onTertiaryContainer: Color(0xFFAFEBF3),
+  error: Color(0xFFF2B8B5),
+  onError: Color(0xFF601410),
+  errorContainer: Color(0xFF8C1D18),
+  onErrorContainer: Color(0xFFF9DEDC),
+  surface: Color(0xFF1C1B1F),
+  onSurface: Color(0xFFE6E1E5),
+  outline: Color(0xFF8C9196),
 );
-
-// https://github.com/flutter/flutter/issues/115912
-extension SurfaceToneColorSchemeExtension on ColorScheme {
-  Color get surfaceDim => const Color(0xffD0D8DF);
-
-  Color get surfaceBright => const Color(0xffE3EDF7);
-
-  Color get surfaceContainerLowest => const Color(0xffEBF5FF);
-
-  Color get surfaceContainerLow => const Color(0xffE0EBF6);
-
-  Color get surfaceContainer => const Color(0xffDCE8F5);
-
-  Color get surfaceContainerHigh => const Color(0xffD8E5F3);
-
-  Color get surfaceContainerHighest => const Color(0xffD0E0F1);
-}
